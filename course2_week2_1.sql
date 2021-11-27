81f10dc5ce89a8885345c7291274ef3d971770f1
Daniel B. Z <42197094+Daniel6628@users.noreply.github.com>
2021-11-26 20:19:28 -0500

    Update course2_week2_1.sql

--- course2_week2_1.sql
+++ course2_week2_1.sql
@@ -41,3 +41,8 @@ CREATE TABLE track_raw
     ORDER BY track.title LIMIT 3;
     
 
+The expected result of this query on your database is:
+track	                       album
+A Boy Named Sue (live)	     The Legend Of Johnny Cash
+A Brief History of Packets	 Computing Conversations
+Aguas De Marco	             Natural Wonders Music Sampler 1999
